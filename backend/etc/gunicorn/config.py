26feb97575fe12156fad77ef41ca2afd4a68815f
jkawczynski <janusz.kawczynski@adverity.com>
2022-09-05 16:28:24 +0200

    Gunicorn fix

--- backend/etc/gunicorn/config.py
+++ backend/etc/gunicorn/config.py
@@ -3,3 +3,4 @@ import multiprocessing
 bind = "unix:/tmp/gunicorn.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = "domomir_backend.wsgi"
+secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'https', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
