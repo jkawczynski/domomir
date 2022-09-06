7c6d1859eeda4488e42c7e607a48271d6e146109
jkawczynski <janusz.kawczynski@adverity.com>
2022-09-06 18:43:42 +0200

    Trainig details table

--- frontend/src/fitness/trainings/components/TrainingDetails.tsx
+++ frontend/src/fitness/trainings/components/TrainingDetails.tsx
@@ -1,9 +1,60 @@
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import moment from "moment";
 import { FunctionComponent } from "react";
-import { Training } from "../../api/models";
+
+import { Training, TrainingExercise } from "../../api/models";
+
+const headers = ["Exercise", "Set number", "Time spent", "Weight", "Reps"];
+
+const ExerciseRow: FunctionComponent<{ exercise: TrainingExercise }> = ({
+  exercise,
+}) => {
+  const timeSpent = moment
+    .duration(moment(exercise.started).diff(moment(exercise.completed)))
+    .humanize(false, { ss: 1 });
+  return (
+    <TableRow
+      key={exercise.id}
+      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    >
+      <TableCell component="th" scope="row">
+        {exercise.name}
+      </TableCell>
+      <TableCell align="right">{exercise.set_number}</TableCell>
+      <TableCell align="right">{timeSpent}</TableCell>
+      <TableCell align="right">{exercise.weight}</TableCell>
+      <TableCell align="right">{exercise.reps}</TableCell>
+    </TableRow>
+  );
+};
 
 export const TrainingDetails: FunctionComponent<{ training: Training }> = ({
   training,
 }) => {
-    return "dupa"
-
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {headers.map((header, index) => (
+              <TableCell align={index > 0 ? "right" : undefined}>
+                {header}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {training.exercises.map((exercise) => (
+            <ExerciseRow exercise={exercise} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
